1dcbfe5a933804dadc7ad6ca9c5e9f5adec76e79
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-26 21:52:15 -0800

    project3

--- game.cpp
+++ game.cpp
@@ -23,12 +23,12 @@ Game::Game() {}
 
 /*********************************************************************
 ** Description:     this function is called from main to get the game
-**                  started. Constant game variables are used to set
-**                  the parameters of the game. This function mainly
-**                  controls the game flow such as calling the
-**                  functions that set the game up, play the game,
-**                  and control whether the game continues or ends.
-**                  This is done by calling the appropriate functions.
+**                  started.
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
 *********************************************************************/
 void Game::playGame() {
 
@@ -38,5 +38,8 @@ void Game::playGame() {
 ** Description:     d
 *********************************************************************/
 void Game::gameFlow() {
+    menu.mainMenu();
+
+
 
 }
