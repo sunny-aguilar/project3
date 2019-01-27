6a5e839aaefd0f69d5917176293f2453b07322a2
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-26 20:32:32 -0800

    project3

---
+++ vampire.hpp
@@ -0,0 +1,20 @@
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            Feb 2019
+** Description:     PROJECT 3
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+*********************************************************************/
+
+#ifndef VAMPIRE_HPP
+#define VAMPIRE_HPP
+
+#endif //PROJECT3_VAMPIRE_HPP
