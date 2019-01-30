1a3dbadbf5f9bad66bc4a6706801ca85ad5509ca
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-29 18:40:14 -0800

    project3

--- harrypotter.cpp
+++ harrypotter.cpp
@@ -1,5 +1,32 @@
-//
-// Created by Sandro on 1/29/2019.
-//
-
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            Feb 2019
+** Description:     PROJECT 3
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+*********************************************************************/
 #include "harrypotter.hpp"
+
+/*********************************************************************
+** Description:     no-args default constructor that sets the member
+**                  variables
+**                  name / attack / defense / armor / strength
+*********************************************************************/
+HarryPotter::HarryPotter() :
+        Character{"Harry Potter",0,0,0,10} {
+    attackDice = new Dice*[2];
+    defenseDice = new Dice*[3];
+}
+
+/*********************************************************************
+** Description:     virtual destructor
+*********************************************************************/
+HarryPotter::~HarryPotter() {}
