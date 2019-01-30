56ed22ee627f501aa2875d74bca427c4cf6d9d65
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-29 18:43:45 -0800

    project3

--- harrypotter.cpp
+++ harrypotter.cpp
@@ -30,3 +30,106 @@ HarryPotter::HarryPotter() :
 ** Description:     virtual destructor
 *********************************************************************/
 HarryPotter::~HarryPotter() {}
+
+/*********************************************************************
+** Description:     d
+*********************************************************************/
+string HarryPotter::getName() {
+    return name;
+}
+
+/*********************************************************************
+** Description:     initializes dice based on character dice
+**                  requirements. Attack dice 2d10, defense 3d6.
+*********************************************************************/
+void HarryPotter::initializeDice() {
+    attackDice[0] = new Dice[2];
+    defenseDice[0] = new Dice[2];
+
+    //initialize attack dice
+    for (int index = 0; index < 2; index++) {
+        attackDice[0][index] = Dice(6);
+    }
+    // initialize defense dice
+    for (int index = 0; index < 2; index++) {
+        defenseDice[0][index] = Dice(6);
+    }
+}
+
+/*********************************************************************
+** Description:     d
+*********************************************************************/
+int HarryPotter::rollDice(std::string action) {
+    int totalRolled = 0;
+    if (action == "attack") {
+        totalRolled += attackDice[0][0].randomInt();
+        totalRolled += attackDice[0][1].randomInt();
+    }
+    else if (action == "defend") {
+        totalRolled += defenseDice[0][0].randomInt();
+        totalRolled += defenseDice[0][1].randomInt();
+    }
+    return totalRolled;
+}
+
+/*********************************************************************
+** Description:     polymorphic function for attacking. Attack value
+**                  is 2d6.
+*********************************************************************/
+void HarryPotter::attackPlayer(Character *defender) {
+    cout << ">>Harry Potter attacks!    -|---" << endl;
+    attack = 0;
+
+    // roll dice
+    attack = rollDice("attack");
+    cout << "Gets ready to deal " << attack << " attack points (1d12)" << endl << endl;
+
+    // send attack value to defender object
+    defender->setAttackVal(attack);
+}
+
+/*********************************************************************
+** Description:     sets the attack damage received
+*********************************************************************/
+void HarryPotter::setAttackVal(int val) {
+    attack = val;
+}
+
+/*********************************************************************
+** Description:     d
+*********************************************************************/
+void HarryPotter::defend() {
+    cout << ">>Harry Potter defends!    )))" << endl;
+    cout << "Strength points " << strength << endl << endl;
+    int defendValue = 0;
+
+    // special defense **Mob**
+    // blue men loses 1 dice for every 4 points of damage
+    specialAbility();
+
+
+    // roll dice
+    defendValue = rollDice("defend");
+
+    // calculate net damage received
+    int damage = attack - defendValue - armor;
+    if (damage < 0) { damage = 0; }
+    cout << "Defense blocked " << defendValue << " attack points\n";
+
+    cout << setw(2) << attack << " - attack points\n";
+    cout << setw(2) << defendValue << " - defense block (1d6)\n";
+    cout << setw(2) << armor << " - armor block\n";
+    cout << setw(2) << damage << " - damage inflicted\n";
+    cout << setw(2) << strength - damage << " - ending strength points\n\n";
+
+    // update player strength
+    damageReceived = damage;
+}
+
+
+
+
+
+
+
+
