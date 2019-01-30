b35386bfe83a3124ec24e7338199f96b1ff808b1
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-29 16:25:21 -0800

    project3

--- bluemen.cpp
+++ bluemen.cpp
@@ -48,7 +48,7 @@ void BlueMen::initializeDice() {
 
     //initialize attack dice
     for (int index = 0; index < 2; index++) {
-        attackDice[0][0] = Dice(10);
+        attackDice[0][index] = Dice(10);
     }
     // initialize defense dice
     for (int index = 0; index < 3; index++) {
@@ -63,12 +63,44 @@ int BlueMen::rollDice(std::string action) {
     int totalRolled = 0;
     if (action == "attack") {
         totalRolled += attackDice[0][0].randomInt();
+        totalRolled += attackDice[0][1].randomInt();
     }
     else if (action == "defend") {
         totalRolled += defenseDice[0][0].randomInt();
+        totalRolled += defenseDice[0][1].randomInt();
+        totalRolled += defenseDice[0][2].randomInt();
     }
     return totalRolled;
 }
+
+/*********************************************************************
+** Description:     polymorphic function for attacking. Attack value
+**                  is 2d10.
+*********************************************************************/
+void BlueMen::attackPlayer(Character *defender) {
+    cout << ">>Blue Men attacks!    -|---" << endl;
+    attack = 0;
+
+    // roll dice
+    attack = rollDice("attack");
+    cout << "Gets ready to deal " << attack << " attack points (1d12)" << endl << endl;
+
+    // send attack value to defender object
+    defender->setAttackVal(attack);
+}
+
+/*********************************************************************
+** Description:     sets the attack damage received
+*********************************************************************/
+void BlueMen::setAttackVal(int val) {
+    attack = val;
+}
+
+
+
+
+
+
 
 
 
