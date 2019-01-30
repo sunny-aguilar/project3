452c8bca3d456b82cbd05266f41addd5d2378b86
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-29 17:09:32 -0800

    project3

--- medusa.cpp
+++ medusa.cpp
@@ -1,5 +1,37 @@
-//
-// Created by Sandro on 1/29/2019.
-//
-
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            Feb 2019
+** Description:     PROJECT 3
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+*********************************************************************/
 #include "medusa.hpp"
+/*********************************************************************
+** Description:     no-args default constructor that sets the member
+**                  variables
+**                  name / attack / defense / armor / strength
+*********************************************************************/
+Medusa::Medusa() :
+    Character{"Medusa",0,0,3,8} {
+    attackDice = new Dice*[2];
+    defenseDice = new Dice*[3];
+}
+
+
+
+
+
+
+
+
+
+
+
--- medusa.hpp
+++ medusa.hpp
@@ -1,8 +1,46 @@
-//
-// Created by Sandro on 1/29/2019.
-//
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            Feb 2019
+** Description:     PROJECT 3
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+**                  d
+*********************************************************************/
+#ifndef MEDUSA_HPP
+#define MEDUSA_HPP
 
-#ifndef PROJECT3_MEDUSA_HPP
-#define PROJECT3_MEDUSA_HPP
+#include "character.hpp"
+#include <iostream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setw;
+
+class Medusa : public Character {
+public:
+    Medusa();
+    ~Medusa();
+    string getName() override;
+    void initializeDice() override;
+    int rollDice(std::string action) override;
+    void attackPlayer(Character *defender) override;
+    void setAttackVal(int val) override;
+    void defend() override;
+    void strengthUpdate() override;
+    void checkStrength() override;
+    bool playerStatus() override;
+    void specialAbility() override;
+
+private:
+
+};
 
 #endif //PROJECT3_MEDUSA_HPP
