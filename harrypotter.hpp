98870328b5fdcc0159190401de31347797338a63
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-29 18:31:38 -0800

    project3

--- harrypotter.hpp
+++ harrypotter.hpp
@@ -2,7 +2,34 @@
 // Created by Sandro on 1/29/2019.
 //
 
-#ifndef PROJECT3_HARRYPOTTER_HPP
-#define PROJECT3_HARRYPOTTER_HPP
+#ifndef HARRYPOTTER_HPP
+#define HARRYPOTTER_HPP
+
+#include "character.hpp"
+#include <iostream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setw;
+
+class HarryPotter : public Character {
+public:
+    HarryPotter();
+    ~HarryPotter() override;
+    string getName() override;
+    void initializeDice() override;
+    int rollDice(std::string action) override;
+    void attackPlayer(Character *defender) override;
+    void setAttackVal(int val) override;
+    void defend() override;
+    void strengthUpdate() override;
+    void checkStrength() override;
+    bool playerStatus() override;
+    void specialAbility() override;
+
+private:
+
+};
 
 #endif //PROJECT3_HARRYPOTTER_HPP
