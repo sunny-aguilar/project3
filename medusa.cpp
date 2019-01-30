aae9b61053c9c4fee138bb4f45cb9c132868701e
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-29 17:06:16 -0800

    project3

---
+++ medusa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sandro on 1/29/2019.
+//
+
+#include "medusa.hpp"
---
+++ medusa.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Sandro on 1/29/2019.
+//
+
+#ifndef PROJECT3_MEDUSA_HPP
+#define PROJECT3_MEDUSA_HPP
+
+#endif //PROJECT3_MEDUSA_HPP
